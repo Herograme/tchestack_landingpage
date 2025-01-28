f7d33aa04befc71fa6656f38b0a36485652b8162
Samuel Borges <Herograme@gmail.com>
2025-01-28 00:47:02 -0300

    Refactor: Update App.vue to use Vue Router view

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
 import Header from './components/Header.vue'
-import HeroSection from './components/HeroSection.vue'
-import TextSection from './components/TextSection.vue'
-import CardsSection from './components/CardsSection.vue'
-import StatsSection from './components/StatsSection.vue'
 import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <Header />
-  <HeroSection />
-  <TextSection />
-  <CardsSection />
-  <StatsSection />
+  <router-view></router-view>
   <Footer />
 </template>
 
