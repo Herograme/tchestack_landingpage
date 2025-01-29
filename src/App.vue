6e1d657379a6927d137517eec19e557385b079e5
Samuel Borges <Herograme@gmail.com>
2025-01-28 23:15:48 -0300

    Chore: Integrate Vercel Analytics component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import { Analytics } from '@vercel/analytics/vue'
 </script>
 
 <template>
+  <Analytics />
   <Header />
   <router-view></router-view>
   <Footer />
